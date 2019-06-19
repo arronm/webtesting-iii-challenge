5ae68bd6334e154d1e8772ea0eb50e3b5f48337b
Arron M <marshall.arron@gmail.com>
2019-06-19 15:43:27 -0700

    Set up initial test todos

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,3 @@
 // Test away
+
+// similar tests to controls spec, but also ensure display updates with values from clicking buttons
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,5 @@
 // Test away!
+
+// defaults to unlocked/open display
+
+// test different render methods by passing in props
