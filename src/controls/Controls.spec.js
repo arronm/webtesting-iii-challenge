e4c7d159867f9e52ec8181694d67b38eab1edf3d
Arron M <marshall.arron@gmail.com>
2019-06-19 08:42:06 -0700

    Set up initial project on local

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+
+// defaults to unlocked/open controls
+
+// expect close available and lock not available
+
+// click close, expect open
+// click close expect lock available
+
+// click lock, expect open unavailable
+// click lock expect unlock available
