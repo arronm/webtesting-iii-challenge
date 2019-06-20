b66a30c3f64a0d0e07fbf7d7101514aeec69dab4
Arron M <marshall.arron@gmail.com>
2019-06-19 21:25:31 -0700

    Set up tests for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import Display from './Display';
 
@@ -8,8 +8,37 @@ describe('<Display />', () => {
     const component = render(<Display />);
     expect(component).toBeTruthy();
   });
+
+  it('should render to unlocked/open by default', () => {
+    const { queryByText } = render(<Display />);
+    const closedDisplay = queryByText(/open/i);
+    const lockedDisplay = queryByText(/unlocked/i);
+
+    expect(closedDisplay).toBeTruthy();
+    expect(closedDisplay.classList).toContain('green-led');
+    expect(lockedDisplay).toBeTruthy();
+    expect(lockedDisplay.classList).toContain('green-led');
+  });
+  
+  it('should render unlocked/closed', () => {
+    const { queryByText } = render(<Display closed={true} />);
+    const closedDisplay = queryByText(/closed/i);
+    const lockedDisplay = queryByText(/unlocked/i);
+    
+    expect(closedDisplay).toBeTruthy();
+    expect(closedDisplay.classList).toContain('red-led');
+    expect(lockedDisplay).toBeTruthy();
+    expect(lockedDisplay.classList).toContain('green-led');
+  });  
+  
+  it('should render locked/closed', () => {
+    const { queryByText } = render(<Display closed={true} locked={true} />);
+    const closedDisplay = queryByText(/closed/i);
+    const lockedDisplay = queryByText(/locked/i);
+    
+    expect(closedDisplay).toBeTruthy();
+    expect(closedDisplay.classList).toContain('red-led');
+    expect(lockedDisplay).toBeTruthy();
+    expect(lockedDisplay.classList).toContain('red-led');
+  });  
 });
-
-// defaults to unlocked/open display
-
-// test different render methods by passing in props
