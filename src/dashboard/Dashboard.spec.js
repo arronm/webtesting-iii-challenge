beb614c70cba1056f4e3abbed41baf4b972b8873
Arron M <marshall.arron@gmail.com>
2019-06-19 21:49:07 -0700

    Add tests for Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import Dashboard from './Dashboard';
 
@@ -8,6 +8,46 @@ describe('<Dashboard />', () => {
     const component = render(<Dashboard />);
     expect(component).toBeTruthy();
   });
-});
 
-// similar tests to controls spec, but also ensure display updates with values from clicking buttons
+  it('should render display correctly when clicking "Close Gate"', () => {
+    const { queryByText } = render(<Dashboard />);
+    const closedBtn = queryByText(/close gate/i);
+    const closedDisplay = queryByText(/open/i);
+    expect(closedBtn).toBeTruthy();
+    expect(closedDisplay).toBeTruthy();
+    expect(closedDisplay.classList).toContain('green-led');
+    
+    const lockedBtn = queryByText(/lock gate/i);
+    const lockedDisplay = queryByText(/unlocked/i);
+    expect(lockedBtn).toBeTruthy();
+    expect(lockedBtn.disabled).toBeTruthy();
+    expect(lockedDisplay).toBeTruthy();
+    expect(lockedDisplay.classList).toContain('green-led');
+    
+    fireEvent.click(closedBtn);
+    expect(closedBtn.textContent).toContain('Open Gate');
+    expect(closedDisplay.classList).toContain('red-led');
+
+    expect(lockedBtn.disabled).toBeFalsy();
+    expect(lockedDisplay.classList).toContain('green-led');
+  });
+
+  it('should render display correctly when clicking "Lock Gate"', () => {
+    const { queryByText } = render(<Dashboard />);
+    const closedBtn = queryByText(/close gate/i);
+    expect(closedBtn).toBeTruthy();
+    fireEvent.click(closedBtn);
+    
+    const closedDisplay = queryByText(/closed/i);
+    const lockedBtn = queryByText(/lock gate/i);
+    const lockedDisplay = queryByText(/unlocked/i);
+    expect(lockedBtn).toBeTruthy();
+    expect(lockedDisplay).toBeTruthy();
+    fireEvent.click(lockedBtn);
+
+    expect(closedBtn.disabled).toBeTruthy();
+    expect(closedDisplay.classList).toContain('red-led');
+    expect(lockedBtn.textContent).toContain('Unlock Gate');
+    expect(lockedDisplay.classList).toContain('red-led');
+  });
+});
